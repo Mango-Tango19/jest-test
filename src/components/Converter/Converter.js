bd53346bfbca3cc60587ee70f491106ab1701471
front_Dasha <64001891+Mango-Tango19@users.noreply.github.com>
2022-10-01 18:57:08 +0300

    add event test

--- src/components/Converter/Converter.js
+++ src/components/Converter/Converter.js
@@ -1,14 +1,32 @@
+import React, { useState } from "react";
 export function Converter() {
+	const [rub, setRub] = useState(2);
+	const [usd, setUsd] = useState(1);
+
 	return (
 		<form>
 			<label>
 				<p>Сумма в рублях</p>
-				<input type='number' name='rub' min='0' step='1' value={2} />
+				<input
+					type='number'
+					name='rub'
+					min='0'
+					step='1'
+					value={rub}
+					onChange={(e) => setRub(e.target.value)}
+				/>
 			</label>
 
 			<label>
-				<p>Сумма в доллрарах</p>
-				<input type='number' name='usd' min='0' step='1' value={32} />
+				<p>Сумма в долларах</p>
+				<input
+					type='number'
+					name='usd'
+					min='0'
+					step='1'
+					value={usd}
+					onChange={(e) => setUsd(e.target.value)}
+				/>
 			</label>
 		</form>
 	);
--- src/components/Converter/Converter.test.js
+++ src/components/Converter/Converter.test.js
@@ -1,14 +1,40 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Converter } from "./Converter";
 
 describe("when rendered", () => {
 	it("rub val should have a value with a rub amount", () => {
 		render(<Converter />);
-		expect(screen.getByAltText(/Сумма в рублях/)).toHaveValue(2);
+		expect(screen.getByLabelText(/Сумма в рублях/)).toHaveValue(2);
 	});
 
-	it("usd val should have a value with a rub amount", () => {
+	it("usd val should have a value with a usd amount", () => {
 		render(<Converter />);
-		expect(screen.getByAltText(/Сумма в долларах/)).toHaveValue(32);
+		expect(screen.getByLabelText(/Сумма в долларах/)).toHaveValue(1);
+	});
+});
+
+describe("when user type in RUB", () => {
+	it("should update value", () => {
+		render(<Converter />);
+		const input = screen.getByLabelText(/Сумма в рублях/);
+		userEvent.clear(input);
+		userEvent.type(input, "2");
+		//expect(input).toHaveValue(2);
+		expect(input).not.toHaveValue("2");
+		expect(input).toHaveDisplayValue("");
+	});
+});
+
+describe("when user type in USD", () => {
+	it("should update value", () => {
+		render(<Converter />);
+		const input = screen.getByLabelText(/Сумма в долларах/);
+		userEvent.clear(input);
+		userEvent.type(input, "1");
+
+		//expect(input).toHaveValue(1);
+		expect(input).not.toHaveValue("1");
+		expect(input).toHaveDisplayValue("");
 	});
 });
