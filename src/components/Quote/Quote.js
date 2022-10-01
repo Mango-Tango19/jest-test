2f08f4be6d8b4ee65fe2c3dcf56d0435cbc11ba5
front_Dasha <64001891+Mango-Tango19@users.noreply.github.com>
2022-10-01 14:01:44 +0300

    add test gor random

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -2,7 +2,8 @@ import React from "react";
 import { quotes } from "./quotes";
 
 export const Quote = () => {
-	const { text, author } = quotes[0];
+	const randomIdx = Math.floor(Math.random() * quotes.length);
+	const { text, author } = quotes[randomIdx];
 	return (
 		<>
 			<blockquote>{text}</blockquote>
