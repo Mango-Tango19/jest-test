7acc8139892aa83ef74184a766d7b3d82f38c1ae
front_Dasha <64001891+Mango-Tango19@users.noreply.github.com>
2022-10-01 19:26:51 +0300

    add tests for the hook

---
+++ src/components/Converter/useConverter.test.js
@@ -0,0 +1,18 @@
+describe("when rendered", () => {
+	it.todo("RUB input should have a val equal to initial RUB amount");
+	it.todo("USD input should have a val equal to calculated USD amount");
+});
+
+describe("when call updateRub method", () => {
+	it.todo("should update RUB val");
+	it.todo("should calculate USD val");
+});
+
+describe("when call updateUsd method", () => {
+	it.todo("should update USD val");
+	it.todo("should recalculate RUB val");
+});
+
+describe("when re-rendered", () => {
+	it.todo("should update its values");
+});
