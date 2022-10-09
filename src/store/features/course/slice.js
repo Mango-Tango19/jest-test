c928dbebfa3234f43b55ed75c87edfac39d4d9e2
front_Dasha <64001891+Mango-Tango19@users.noreply.github.com>
2022-10-09 13:28:47 +0300

    add thunk

--- src/store/features/course/slice.js
+++ src/store/features/course/slice.js
@@ -1,10 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-const initialCourseState = { value: 42 };
+const initialCourseState = { value: 42, status: "idle", error: null };
+
+export const fetchTodayCourse = createAsyncThunk(
+	"course/fetchCourse",
+	async () => {
+		const res = await fetchCourse();
+		return res.course;
+	}
+);
 
 const courseSlice = createSlice({
 	name: "course",
 	initialState: initialCourseState,
+	extraReducers: {
+		[fetchTodayCourse.pending]: (state) => state,
+		[fetchTodayCourse.fulfilled]: (state) => state,
+		[fetchTodayCourse.error]: (state) => state,
+	},
 });
 
 export const selectCourse = (state) => state.course.value;
